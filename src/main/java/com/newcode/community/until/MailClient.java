5f4655ae10bc6793c49661fc350c73153de459a3
niguang <1254572621@qq.com>
2024-02-03 19:22:15 +0800

    加入邮箱功能

--- src/main/java/com/newcode/community/until/MailClient.java
+++ src/main/java/com/newcode/community/until/MailClient.java
@@ -1,7 +1,5 @@
 package com.newcode.community.until;
 
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +7,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
-
-import javax.mail.MessageRemovedException;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-
 
 @Component
 public class MailClient {
