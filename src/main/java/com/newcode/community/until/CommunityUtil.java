321e0dc8ace6aa94bda8c1a6dbd7228ba3cfc6bd
niguang <1254572621@qq.com>
2024-02-04 15:35:00 +0800

    添加注册功能

---
+++ src/main/java/com/newcode/community/until/CommunityUtil.java
@@ -0,0 +1,16 @@
+package com.newcode.community.until;
+
+import java.util.UUID;
+
+public class CommunityUtil {
+
+    //生成随机字符串
+    public static String generateUUID(){
+        return UUID.randomUUID().toString().replaceAll("-","");
+    }
+
+    //MD5加密
+    public static String (){
+
+    }
+}
