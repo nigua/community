55b62c4aa173563a4b497588af6ffc51cfe4f73f
niguang <1254572621@qq.com>
2024-02-18 16:35:43 +0800

    点赞

--- src/main/java/com/newcode/community/controller/HomeController.java
+++ src/main/java/com/newcode/community/controller/HomeController.java
@@ -4,7 +4,9 @@ import com.newcode.community.entity.DiscussPost;
 import com.newcode.community.entity.Page;
 import com.newcode.community.entity.User;
 import com.newcode.community.service.DiscussPostService;
+import com.newcode.community.service.LikeService;
 import com.newcode.community.service.UserService;
+import com.newcode.community.until.CommunityConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,13 +20,16 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class HomeController {
+public class HomeController implements CommunityConstant {
 
     @Autowired
     private DiscussPostService discussPostService;
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private LikeService likeService;
 
 
     @RequestMapping(path = "/index" ,method = RequestMethod.GET)
@@ -42,6 +47,10 @@ public class HomeController {
                 map.put("post",post);
                 User user = userService.findUserById(post.getUserId());
                 map.put("user",user);
+
+                long likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_POST, post.getId());
+                map.put("likeCount",likeCount);
+
                 discussPosts.add(map);
             }
         }
